edfc5950e1eb28a44b7ba886eb6a7fffc8b046bf
leovsouzza007 <leonardovsouzza@gmail.com>
2025-09-13 13:22:05 -0300

    corrigindo

--- guardar.php
+++ guardar.php
@@ -4,14 +4,14 @@ $frase1 = $_REQUEST["frase1"];
 $frase2 = $_REQUEST["frase2"];
 $frase3 = $_REQUEST["frase3"];
 
-$arquvio = "arquivo.txt";
+$arquivo = "arquivo.txt";
 
-$frases = "frase1./nfrase2.,/n frase3./n";
+$frases = $frase1 . "\n" . $frase2 . "\n" . $frase3 . "\n";
 
 if (file_put_contents($arquivo, $frases) == false) {
     echo "Frases salvas com sucesso!!";
-    echo'<a href="ler_frases.php">clique aqui para ver o conteudo</a>';
-
+    echo '<a href="ler_frases.php">Clique aqui para ver o conteúdo</a>';
 } else {
-    echo 'erro ao criar o arquivo';
+    echo "Erro ao criar o arquivo";
 }
+?>
--- ler.php
+++ ler.php
@@ -1,11 +1,11 @@
 <?php
 
-$arquvio = "arquivo.txt";
+$arquivo = "arquivo.txt";
 
 if (file_exists($arquvio)) {
     $conteudo = file_get_contents($arquvio);
     echo $conteudo;
 } else {
     echo "<h2>O arquivo não existe. Grave as frases primeiro.</h2>";
-    echo '<a href="gravar.php">Clique aqui para gravar</a>';
+    echo '<a href="formulario.html">Clique aqui para gravar</a>';
 }
